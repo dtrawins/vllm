0fdd3a42b305b8d97aaf4651c6d829ba52985032
Dariusz Trawinski <dariusz.trawinski@intel.com>
2024-07-02 11:12:20 +0200

    ignore_eos

--- benchmarks/backend_request_func.py
+++ benchmarks/backend_request_func.py
@@ -236,6 +236,7 @@ async def async_request_openai_completions(
             "temperature": 0.0,
             "best_of": request_func_input.best_of,
             "max_tokens": request_func_input.output_len,
+            "ignore_eos": True,
             "stream": True,
         }
         headers = {
